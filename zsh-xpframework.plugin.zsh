68184a8fe35f5f7465ce6156ba9899465e77fd37
Sebastian Kusnier <sebastian@kusnier.net>
2012-07-01 19:18:43 +0200

    Add infinitest function to watch for changes in source and run unittest
    - Watched directories: etc, src and xsl

--- zsh-xpframework.plugin.zsh
+++ zsh-xpframework.plugin.zsh
@@ -1,1 +1,6 @@
-
+function infinitest() {
+  inotifywait -e modify -e move -e create -e delete \
+    -r -m src/ etc/ xsl/  | while read line ; do
+      unittest -l xp.unittest.XTermTitleListener - unittest.ini
+  done
+}
