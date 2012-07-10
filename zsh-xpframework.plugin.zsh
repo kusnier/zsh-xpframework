661e226e943eb62bebb2f1e66c947a5b8cf3f966
Sebastian Kusnier <sebastian@kusnier.net>
2012-07-10 12:03:53 +0200

    Remove listener
    - xp 5.8.6: Unittest output is colored if a capable terminal window is detected

--- zsh-xpframework.plugin.zsh
+++ zsh-xpframework.plugin.zsh
@@ -1,7 +1,7 @@
 function infinitest() {
   inotifywait -e modify -e move -e create -e delete \
     -r -m src/ etc/ xsl/  | while read line ; do
-      unittest -l xp.unittest.XTermTitleListener - unittest.ini
+      unittest unittest.ini
   done
 }
 
